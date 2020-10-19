0dc4eac5b5202a9685310dad9424757519375e32
W Lawless <wlawless.sharpe@protonmail.com>
2020-10-18 20:30:01 -0400

    Initial Commit

---
+++ taskbook/taskbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskbookApp.swift
+//  taskbook
+//
+//  Created by W Lawless on 10/18/20.
+//
+
+import SwiftUI
+
+@main
+struct taskbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
